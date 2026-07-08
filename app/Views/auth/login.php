0b476626a14f10d1ac2fa8b3afb34a293357d1a1
KingCarl <carlwanke2003@gmail.com>
2026-07-08 16:18:56 -0500

    Update login.php

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -10,12 +10,6 @@
  * 3. Sabando Varela Angello Michael  -> Usuario: 'sabando_angello'
  * 4. Wanke Cedeño Carl Hermann       -> Usuario: 'wanke_carl'
  * 5. Icaza Lino Diana Valentina      -> Usuario: 'icaza_diana'
- * * 🥼 CUENTAS DE MÉDICOS DE PRUEBA:
- * 1. Dr. Carlos Andrade  -> Usuario: 'andrade_carlos'
- * 2. Dra. María Cevallos -> Usuario: 'cevallos_maria'
- * 3. Dr. Jorge Mendoza   -> Usuario: 'mendoza_jorge'
- * 4. Dra. Ana Guerrero   -> Usuario: 'guerrero_ana'
- * 5. Dr. Luis Palacios   -> Usuario: 'palacios_luis'
  * ==========================================================================
  */
 ?>
